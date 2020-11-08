b0bce684aee2240307190f35e66ea94fbde22090
healy218 <mrhealy115@gmail.com>
2020-11-08 17:46:49 -0600

    Created a server

--- starter/index.js
+++ starter/index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-
+const http = require('http');
 
 //Blocking, Syncronous way to do it
 // const textin = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -10,10 +10,25 @@ const fs = require('fs');
 
 //Non-blocking, a-synchronous way
 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-        console.log(data2);
-    });
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//             console.log(data3);
+//             fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err =>{
+//                 console.log('Youre File has been written');
+//             });
+//         });
+//     });
+// });
+
+// console.log("Will write data!");
+
+
+const server = http.createServer((req, res) =>{
+    res.end('Hello from the server');
 });
 
-console.log("Will write data!");
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000');
+})
