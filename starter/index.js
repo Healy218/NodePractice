fc1781da95c7c400da6b9cc4cac4153faef1f614
healy218 <mrhealy115@gmail.com>
2020-11-08 17:20:10 -0600

    Learning async

--- starter/index.js
+++ starter/index.js
@@ -1,11 +1,19 @@
 const fs = require('fs');
 
-const textin = fs.readFileSync('./txt/input.txt', 'utf-8');
 
-const textOut = `This is what we know about the avacado: ${textin}. \n Created on ${Date.now()}`;
+//Blocking, Syncronous way to do it
+// const textin = fs.readFileSync('./txt/input.txt', 'utf-8');
+// const textOut = `This is what we know about the avacado: ${textin}. \n Created on ${Date.now()}`;
+// console.log(textOut);
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log("File Written!");
 
-console.log(textOut);
+//Non-blocking, a-synchronous way
 
-fs.writeFileSync('./txt/output.txt', textOut);
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+    });
+});
 
-console.log("File Written!");
+console.log("Will write data!");
