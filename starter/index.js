39440d5e49c0d1bdce7ae3455697e0eb42f70a77
healy218 <mrhealy115@gmail.com>
2020-11-11 15:17:41 -0600

    Set up the Node Farm site

--- starter/index.js
+++ starter/index.js
@@ -23,10 +23,55 @@ const http = require('http');
 // });
 
 // console.log("Will write data!");
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID}/g, product.id);
 
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not_organic');
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) =>{
-    res.end('Hello from the server');
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview') {
+        res.writeHead(200, { 'Content-type': 'text/html'});
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el));
+        res.end(tempOverview);
+    }
+
+    //PRODUCT
+    else if(pathName === '/product') {
+        res.end('this is the product');
+    }
+
+    //API
+    else if(pathName === '/api') {
+            res.writeHead(200, { 'Content-type': 'application/json'});
+            res.end(data);
+    }
+
+    //NOT FOUND
+    else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'yo-yo': 'Im really cool',
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
