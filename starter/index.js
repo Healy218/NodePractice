83052733b6df902b2457bf2e7b103f0a14430543
healy218 <mrhealy115@gmail.com>
2020-11-12 13:21:23 -0600

    Finished Node Farm Project

--- starter/index.js
+++ starter/index.js
@@ -3,6 +3,8 @@ const http = require('http');
 const url  = require('url');
 const replaceTemplate = require('./modules/replaceTemplate');
 
+const slugify = require('slugify');
+
 //Blocking, Syncronous way to do it
 // const textin = fs.readFileSync('./txt/input.txt', 'utf-8');
 // const textOut = `This is what we know about the avacado: ${textin}. \n Created on ${Date.now()}`;
@@ -31,6 +33,7 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
 
 const server = http.createServer((req, res) =>{
     
--- starter/modules/replaceTemplate.js
+++ starter/modules/replaceTemplate.js
@@ -4,10 +4,10 @@ module.exports = (temp, product) => {
     output = output.replace(/{%QUANTITY%}/g, product.quantity);
     output = output.replace(/{%PRICE%}/g, product.price);
     output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%NUTRIENT%}/g, product.nutrients);
     output = output.replace(/{%DESCRIPTION%}/g, product.description);
     output = output.replace(/{%ID%}/g, product.id);
 
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not_organic');
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
     return output;
 }
